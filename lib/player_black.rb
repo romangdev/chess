4909d9f087c4b3bc9de87f6f3babd9c8453f9893
Roman Geathers <romangeathersdev@gmail.com>
2022-07-24 14:05:03 -0400

    Add on black movement code into main file

--- lib/player_black.rb
+++ lib/player_black.rb
@@ -3,10 +3,12 @@
 require_relative 'chess_pieces'
 
 class PlayerBlack
+  attr_reader :player_pieces, :player_color
+  
   include ChessPieces
 
   def initialize
-    @player_color = "white"
+    @player_color = "black"
     @player_pieces = [BLACK_PAWN, BLACK_ROOK, BLACK_KNIGHT,
                       BLACK_BISHOP, BLACK_QUEEN, BLACK_KING]
   end
--- main.rb
+++ main.rb
@@ -70,6 +70,58 @@ board.chess_board[player_end[0]][player_end[1]] = hold_start
 
 board.display_board
 
+# player black turn
+confirmed = false
+until confirmed
+  flag = false 
+  until flag
+    player_choice = game.get_player_location(player_black.player_color)
+    player_choice = game.convert_player_location(player_choice)
+    flag = game.verify_location_piece(player_black.player_pieces, player_choice, board.chess_board)
+    puts "Your piece isn't located there, please try again!" if flag == false
+  end
 
+  board.display_board(player_choice)
+  piece_confirm = game.get_piece_choice_confirm
+  confirmed = game.handle_confirm_choice(piece_confirm)
+
+  if confirmed == false
+    player_choice = nil 
+    board.display_board
+    puts "Piece movement aborted. Pick another piece."
+  end
+end
+
+piece_to_move = board.chess_board[player_choice[0]][player_choice[1]]
+possible_moves = piece_to_move.generate_moves(player_choice, board.chess_board, piece_to_move.piece_symbol)
+puts "\n"
+board.display_board(player_choice, possible_moves)
+
+confirmed = false
+until confirmed
+  flag = false 
+  until flag
+    player_end = game.get_end_location(player_black.player_color)
+    player_end_hold = player_end.join("")
+    player_end = game.convert_player_location(player_end)
+    flag = game.verify_possible_move(possible_moves, player_end)
+    puts "You can't move there! Try again..." if flag == false
+  end
+
+  end_confirm = game.get_end_move_confirm(player_end_hold)
+  confirmed = game.handle_confirm_choice(end_confirm)
+
+  if confirmed == false
+    player_end = nil 
+    board.display_board(player_choice, possible_moves)
+    puts "Move choice aborted. Pick another move."
+  end
+end
+
+hold_start = board.chess_board[player_choice[0]][player_choice[1]]
+board.chess_board[player_choice[0]][player_choice[1]] = "   "
+board.chess_board[player_end[0]][player_end[1]] = hold_start
+
+board.display_board
 
 
